370c966167621136a43552c57724a78d5d40c88e
johannadee <158396571+johannadee@users.noreply.github.com>
2024-02-15 09:55:54 +0800

    Create ulla.py

---
+++ Activity 6/ulla.py
@@ -0,0 +1,32 @@
+import os
+
+if not os.path.exists('thinkdsp.py'):
+    !wget https://github.com/AllenDowney/ThinkDSP/raw/master/code/thinkdsp.py
+from thinkdsp import TriangleSignal
+from thinkdsp import decorate
+
+signal = TriangleSignal(50)
+duration = signal.period*2
+segment = signal.make_wave(duration, framerate=20000)
+segment.plot()
+decorate(xlabel='Time (s)')
+wave = signal.make_wave(duration=0.5, framerate=10000)
+wave.apodize()
+wave.make_audio()
+spectrum = wave.make_spectrum()
+spectrum.plot()
+decoratefrom thinkdsp import SquareSignal
+(xlabel='Frequency (Hz)')
+
+signal = SquareSignal(300)
+duration = signal.period*2
+segment = signal.make_wave(duration, framerate=30000)
+segment.plot()
+decorate(xlabel='Time (s)')
+wave = signal.make_wave(duration=0.5, framerate=10000)
+wave.apodize()
+wave.make_audio()
+spectrum = wave.make_spectrum()
+spectrum.plot()
+decorate(xlabel='Frequency (Hz)')
+
